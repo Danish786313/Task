89fd1e219c00be3c264f6dea55153fb7474ec5ea
Tabassum2276 <95295798+Tabassum2276@users.noreply.github.com>
2023-02-01 23:24:09 +0530

    working on it

--- migrations/20230201160315-create-task.js
+++ migrations/20230201160315-create-task.js
@@ -10,7 +10,13 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model : {
+            tableName : 'Users'
+          },
+          key: 'id'
+        },
       },
       taskname: {
         type: Sequelize.STRING
